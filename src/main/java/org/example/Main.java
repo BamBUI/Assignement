e5f2925eec1fb4231530bf36d5ef3f8d2fc51103
p.mironov <p.mironov@kg.ru>
2024-10-18 17:19:43 +0400

    Switched to "Allman" style Deleted unnecessary spaces.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
-
-
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Hello world from " + name);
