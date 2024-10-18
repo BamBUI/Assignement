002cb5d223018fb7ada79f1fbebf54426fbb5bf6
p.mironov <p.mironov@kg.ru>
2024-10-18 17:20:07 +0400

    Added comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Comment
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("Hello world from " + name);
