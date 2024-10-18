d5e889fd2e4335a8992c3f2aa90aea7ea7265ca0
p.mironov <p.mironov@kg.ru>
2024-10-18 17:09:29 +0400

    Added String "name" and Scanner.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Scanner terminalInput = new Scanner(System.in);
+        String name = terminalInput.nextLine();
+        System.out.println("Hello world from " + name);
     }
 }
